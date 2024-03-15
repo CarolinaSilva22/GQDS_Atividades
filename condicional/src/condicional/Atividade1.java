7e233563694870e296f2560a48b60e3442e4a2ef
CarolinaSilva <142807604+CarolinaSilva22@users.noreply.github.com>
2024-03-15 14:00:42 -0300

    Update Atividade1.java

--- condicional/src/condicional/Atividade1.java
+++ condicional/src/condicional/Atividade1.java
@@ -10,46 +10,42 @@
 			// TODO Auto-generated method stub
 
 		    Scanner input = new Scanner(System.in);
-				
-			System.out.println("Digite sua idade: ");
-			int idade = input.nextInt();
-			
-			System.out.println("Digite seu sexo(M/F): ");
-			String sexo = input.next();
-			
-			switch(sexo){
-			case "M":
-			    if(idade <= 15){
-			    	System.out.println("valor mensalidade: R$ 60,00");
-		     	}
-			    if(idade >= 16 && idade <=18){
-			    	System.out.println("valor mensalidade: R$ 75,00");
-		     	}
-			    if(idade >= 19 && idade <=30){
-			    	System.out.println("valor mensalidade: R$ 90,00");
-			    }
-			    if(idade >= 31 && idade <=40){
-			    	System.out.println("valor mensalidade: R$ 85,00");		
-				}
-			    if(idade > 40){
-			    	System.out.println("valor mensalidade: R$ 80,00");
-				};
-			break;
-			case "F":
-				 if(idade <= 18){
-				    System.out.println("valor mensalidade: R$ 60,00");
-			     }
-				 if(idade >= 19 && idade <=25){
-				    System.out.println("valor mensalidade: R$ 90,00");
-			     }
-				 if(idade >= 26 && idade <=40){
-				    System.out.println("valor mensalidade: R$ 85,00");
-				 }
-				 if(idade > 40){
-				    System.out.println("valor mensalidade: R$ 80,00");
-				 };
-			break;
-			default : System.out.println("Valor Errado!!");
+		 System.out.println("Digite seu sexo(1-Masculino/2-Feminino):");
+		 int sexo = input.nextInt();
+
+		 System.out.println("Digite sua idade:");
+		 int idade = input.nextInt();
+
+		if(sexo == 1) {
+			if(idade <= 15) {
+				System.out.println("Mensalidade a ser paga:R$ 60");
+			}
+			 else if(idade>=16 && idade<=18) {
+                        	System.out.println("Mensalidade a ser paga:R$ 75");
 			 }
-	}
+			 else if(idade>=19 && idade<=30) {
+            	                System.out.println("Mensalidade a ser paga:R$ 90");
+                         }
+			 else if(idade>=31 && idade<=40) {
+            	                System.out.println("Mensalidade a ser paga:R$ 85");
+                         }
+			 else if(idade>40) {
+            	                System.out.println("Mensalidade a ser paga:R$ 80");
+                        }
+		}
+		else if(sexo == 2) {
+		    if(idade <= 18) {
+            	               System.out.println("Mensalidade a ser paga:R$ 60");
+			}
+			 else if(idade>=19 && idade<=25) {
+            	               System.out.println("Mensalidade a ser paga:R$ 90");
+                        }
+			 else if(idade>=26 && idade<=40) {
+             	               System.out.println("Mensalidade a ser paga:R$ 85");
+                        }
+		   	 else if(idade>40) {
+            	               System.out.println("Mensalidade a ser paga:R$ 80");
+                        }
+	       }
+}
 }
